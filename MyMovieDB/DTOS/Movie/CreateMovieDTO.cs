03955a1e557e18714aabd467ab42c0ff9a8fad96
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-11-19 01:26:46 -0400

    Add error message to category id

--- MyMovieDB/DTOS/Movie/CreateMovieDTO.cs
+++ MyMovieDB/DTOS/Movie/CreateMovieDTO.cs
@@ -5,5 +5,6 @@ namespace MyMovieDB.DTOS;
 public sealed class CreateMovieDTO : MovieDTO
 {
     [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Category ID is required")]
     public int CategoryId { get; set; }
 }
